4769e82aa658882ce550492db87635a18f6864f1
saiarf <aarriiff134@gmail.com>
2022-02-13 19:27:30 +0700

    menambahkan 2 file python

---
+++ Python-Test.py
@@ -0,0 +1,11 @@
+print("Hello World")
+print("Nama Saya: Muhammad Saiful Arif")
+
+a=5
+b=2
+
+print("Nilai A: ",a)
+print("Nilai B: ",b)
+print("Nilai A + Nilai B = ",a+b)
+
+print("pangkat dari A dan B adalah ",pow(a,b))
